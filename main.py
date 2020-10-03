8f987cf6139459f3dd568cc6eb90f0476fdfbd48
Khairul Fikri <48195138+keruliki@users.noreply.github.com>
2020-10-03 06:58:19 +0100

    full change

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
 buzzer = 18
-led = 27
+led = 24
 
 GPIO.setup(buzzer,GPIO.OUT)
 GPIO.setup(led,GPIO.OUT)
@@ -59,42 +59,48 @@ GPIO_ECHO = 21
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
+object_p = 0
+
 def distance():
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
  
     # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
+    sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
  
-    StartTime = time.time()
-    StopTime = time.time()
+    StartTime = time()
+    StopTime = time()
  
     # save StartTime
     while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
+        StartTime = time()
  
     # save time of arrival
     while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
+        StopTime = time()
  
     # time difference between start and arrival
     TimeElapsed = StopTime - StartTime
     # multiply with the sonic speed (34300 cm/s)
     # and divide by 2, because there and back
     distance = (TimeElapsed * 34300) / 2
-    
-    print(distance)
  
     return distance
+def warningh():
+    mylcd.lcd_clear()
+    mylcd.lcd_display_string("Warning!", 1)
+    mylcd.lcd_display_string("Humidity Exceed", 2)
+    GPIO.output(led, True)	
+    sleep(1)
+    
 
 def warning():
-	mylcd.lcd_clear()
-	mylcd.lcd_display_string("Warning!", 1)
-	GPIO.output(led, True)
-
-
-	
+    mylcd.lcd_clear()
+    mylcd.lcd_display_string("Warning!", 1)
+    mylcd.lcd_display_string("Temperature Exceed", 2)
+    GPIO.output(led, True)	
+    sleep(1)
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
@@ -106,18 +112,34 @@ while True:
     
     dist = distance()
     print ("Measured Distance = %.1f cm" % dist)
-    time.sleep(1)
+    sleep(1)
+    
+    if dist < 100:
+        object_p = 1
+    
+    else:
+        object_p = 0
+        
+    if temperature > 5:
+        warning()
+    
+    if humidity > 85:
+        warningh()
+    
+    
+        
     
     data = {
         "humidity": humidity,
         "temperature" : temperature,
-        "object": "1"
+        "object": object_p
 	}
     
     db.update(data)
     
     display_t = "Temp:{0:0.1f}*C".format(temperature)
     display_h = "Humid:{0:0.1f}% ".format(humidity)
+    mylcd.lcd_clear()
     mylcd.lcd_display_string(display_t, 1)
     mylcd.lcd_display_string(display_h, 2)
     sleep(1)
